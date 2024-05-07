492594ee72e5ab7350422dd2ed898a2335464c87
Walltricker <116916334+Walltricker@users.noreply.github.com>
2024-05-07 19:08:02 +0000

    new changes - going to make a new branch

--- button.js
+++ button.js
@@ -1,17 +1,51 @@
-var id = null;
-function myMove() {
-  var elem = document.getElementById('window');   
-  var pos = 0;
-  clearInterval(id);
-  id = setInterval(frame, 10);
-  function frame() {
-    if (pos == -100) {
-      clearInterval(id);
-    } else {
-      pos--; 
-      elem.style.top = pos + 'vh'; 
-    }
+
+/*══════════════════════════════════════════════════════════════════════
+    JS - Variables
+══════════════════════════════════════════════════════════════════════*/
+var isPhone = false;  // Sets variable as phone - false
+
+
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Function to determine whether PC or Phone
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    isPhone = true;
+  } else {
+    isPhone =false
+  }
+  console.log(`Isphone = ${isPhone}`)
+}
+
+// Create a MediaQueryList object
+var x = window.matchMedia("(max-width: 600px)")
+
+// Call listener function at run time
+myFunction(x);
+
+// Attach listener function on state changes
+x.addEventListener("change", function() {
+  myFunction(x);
+});
+
+
+function buttonCheck(name,type) {
+  if (!isPhone) {
+    pcButton(name,type)
+  }
+  else {
+    phoneButton(name,type)
   }
 }
 
-/* */
+function phoneButton (name,type) {
+  console.log(`The Button ${name} was pressed on the phone and it is a ${type} `)
+}
+function pcButton (name,type) {
+  console.log(`The Button ${name} was pressed on the PC and it is a ${type} `)
+}
+
+
+
+
